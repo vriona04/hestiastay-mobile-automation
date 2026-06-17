19f9112abd721562c8b5bb3ca320da0c79237b46
Ajay Kumar <vriona04@gmail.com>
2026-06-17 17:07:14 +0530

    Enable full regression suite with 12 passing tests

--- tests/test_raise_ticket_e2e.py
+++ tests/test_raise_ticket_e2e.py
@@ -13,14 +13,27 @@ def test_raise_ticket_e2e(driver):
     nav.go_support_tickets()
     time.sleep(3)
 
-    if "Create Ticket" not in driver.page_source:
+    page = driver.page_source
+
+    if "Raise Ticket" in page:
+        driver.find_element(
+            "xpath",
+            "//*[contains(@content-desc,'Raise Ticket')]"
+        ).click()
+        time.sleep(3)
+
+    page = driver.page_source
+
+    if "Create Ticket" not in page:
         pytest.skip("Create Ticket screen not opened")
 
     ticket.enter_title("Automation WiFi Issue")
     ticket.select_wifi_category()
     ticket.verify_category_selected()
     ticket.select_medium_priority()
-    ticket.enter_description("This ticket was created using Appium automation testing.")
+    ticket.enter_description(
+        "This ticket was created using Appium automation testing."
+    )
 
     ticket.submit_ticket()
     ticket.verify_ticket_submitted()
